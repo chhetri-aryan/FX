52f90ef5e45335f5d13dd9614d9a842185ec0046
Aryan Chhetri <aryanjr53@gmail.com>
2024-07-13 11:52:45 +0530

    fixed the sql query

--- src/main/java/Dao/UserDaoImpl.java
+++ src/main/java/Dao/UserDaoImpl.java
@@ -19,7 +19,7 @@ public class UserDaoImpl implements UserInfoDao {
 
 //            ResultSet rs = stm.executeQuery("Select * from product");
             String sql = "INSERT INTO user (name, email, department, phone) VALUES" +
-                    " (" + user.getName() + ",'" + user.getEmail() + "','" + user.getDepartment() + "','" + user.getPhone() +"');";
+                    " ('" + user.getName() + "','" + user.getEmail() + "','" + user.getDepartment() + "','" + user.getPhone() +"');";
 
             System.out.println(sql);
             stm.execute(sql);
