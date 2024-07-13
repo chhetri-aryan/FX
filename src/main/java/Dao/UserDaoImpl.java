13760742a084770baf3bc6ef72a5d7c77118e803
Aryan Chhetri <aryanjr53@gmail.com>
2024-07-13 11:14:21 +0530

    Done with Fx, now working on Dao and database

--- src/main/java/Dao/UserDaoImpl.java
+++ src/main/java/Dao/UserDaoImpl.java
@@ -1,11 +1,34 @@
 package Dao;
 
 import User.User;
+import Util.DbConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UserDaoImpl implements UserInfoDao {
     @Override
     public boolean addUser(User user) {
+        try {
+            Connection con  = DbConnection.getConnection();
+            Statement stm = con.createStatement();
 
+//            ResultSet rs = stm.executeQuery("Select * from product");
+            String sql = "INSERT INTO product (name, price, category) VALUES" +
+                    " (" + user.getName() + "," + user.getEmail() + "," + user.getDepartment() + "," + user.getPhone() +");";
+
+            stm.execute(sql);
+
+
+
+
+            con.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         return false;
     }
