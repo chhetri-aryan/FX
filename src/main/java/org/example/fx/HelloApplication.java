8ff0c30aa47e20f66a60e5f30d6641ce5d3222f6
Aryan Chhetri <aryanjr53@gmail.com>
2024-07-13 11:53:17 +0530

    fixed the sql query

--- src/main/java/org/example/fx/HelloApplication.java
+++ src/main/java/org/example/fx/HelloApplication.java
@@ -3,13 +3,10 @@ package org.example.fx;
 import Dao.UserDaoImpl;
 import User.User;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
